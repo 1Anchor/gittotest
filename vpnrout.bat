542884797ffa8b704ee40243aa6ef9f46b7106e5
1Anchor <flames999@mail.ru>
2020-12-22 20:59:58 +0300

    test btanch css

--- vpnrout.bat
+++ vpnrout.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %systemroot%\system32 
-start rasdial WorkVPN name password1
+start rasdial WorkVPN name password
 timeout /t 5 /nobreak >nul 2>nul
 ipconfig | find "10.100.0.%" > ip.txt
 for /f "tokens=1,2 delims=:" %%i in (ip.txt) do (
